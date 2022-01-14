aa05f9cc5312db9faa8d2d283bd199007ca2ba6d
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2022-01-14 19:24:57 +0700

    removed diiscussion page and logiin button

--- src/components/Navbar/constant.jsx
+++ src/components/Navbar/constant.jsx
@@ -2,6 +2,5 @@ export const menus = [
   { url: "/", label: "Home" },
   { url: "/issue", label: "Issue" },
   { url: "/activities", label: "Activities" },
-  { url: "/discussion", label: "Discussion" },
   { url: "/about", label: "About" },
 ]
